f3064d4cc8e772c8a7acf5bfe42e157fd28f590a
min5ol <sol0508@kakao.com>
2025-04-25 14:04:16 +0900

    4/25 class

---
+++ CloneCoding/waggy/js/dropdown.js
@@ -0,0 +1,8 @@
+document.querySelector('.drop-btn').onclick = (e) => {
+  e.stopPropagation();
+  document.querySelector('.dropdown').classList.toggle('show');
+};
+
+window.onclick = () => {
+  document.querySelector('.dropdown').classList.remove('show');
+}
